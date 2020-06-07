8d3b1abb2e8eef21b7a7aea7a36819a4f0a52301
wonsim00 <womsim00@users.noreply.github.com>
2020-06-07 20:24:33 +0900

    edit spec_gen.py to track gif files

--- spec_gen.py
+++ spec_gen.py
@@ -1,10 +1,22 @@
+import os
+import re
+
 if __name__ == '__main__':
-    path = __file__.split("\\")
+    path = os.path.abspath(__file__).split("\\")
+    print(path)
     if path[-2] != 'hinata_maker':
         raise RuntimeError("Please place this code to \"hinata_maker\" folder!")
     path[0] = path[0].upper()       # for windows
     path = "\\\\".join(path[:-2])   # for windows
 
+    datas = []
+    MATCH = re.compile("^hajime_.*\.gif$")
+    DATA_FORMAT = "('sprites\\{file}', '{path}\\sprites\\{file}', 'DATA'),"
+    for file in os.listdir("./sprites"):
+        if MATCH.match(file):
+            datas.append(DATA_FORMAT.format(path=path, file=file))
+
     with open("./hinata_maker.spec.prototype", "r") as f_read:
         with open("./hinata_maker.spec", "w") as f_write:
-            f_write.write(f_read.read().format(path = path))
+            f_write.write(f_read.read().format(
+                path = path, datas = "\n    ".join(datas)))
