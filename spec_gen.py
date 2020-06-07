5969576cc9d67e804dc702e1d33276a07e242a32
wonsim00 <womsim00@users.noreply.github.com>
2020-06-07 20:27:39 +0900

    bugfix

--- spec_gen.py
+++ spec_gen.py
@@ -3,7 +3,6 @@ import re
 
 if __name__ == '__main__':
     path = os.path.abspath(__file__).split("\\")
-    print(path)
     if path[-2] != 'hinata_maker':
         raise RuntimeError("Please place this code to \"hinata_maker\" folder!")
     path[0] = path[0].upper()       # for windows
@@ -11,7 +10,7 @@ if __name__ == '__main__':
 
     datas = []
     MATCH = re.compile("^hajime_.*\.gif$")
-    DATA_FORMAT = "('sprites\\{file}', '{path}\\sprites\\{file}', 'DATA'),"
+    DATA_FORMAT = "('sprites\\{file}', '{path}\\hinata_maker\\sprites\\{file}', 'DATA'),"
     for file in os.listdir("./sprites"):
         if MATCH.match(file):
             datas.append(DATA_FORMAT.format(path=path, file=file))
